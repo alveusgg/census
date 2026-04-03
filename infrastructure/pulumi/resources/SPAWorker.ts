b7bba38db81c48cd4e5204f97c8b961aeaba8af2
James Williams <james.williams-con@mhr.co.uk>
2026-04-03 19:17:08 +0100

    Add SPA worker runtime config endpoint

---
+++ infrastructure/pulumi/resources/SPAWorker.ts
@@ -0,0 +1,95 @@
+import { Buffer } from 'node:buffer';
+import { ComponentResource, Input, interpolate, jsonStringify, output, Output, ResourceOptions } from '@pulumi/pulumi';
+
+export interface SPAWorkerArgs {
+  account_id: Input<string>;
+  name: Input<string>;
+  env: string;
+  route: Input<string>;
+  assetsDirectory: Input<string>;
+  backstage: {
+    variables: {
+      apiBaseUrl: Input<string>;
+      ipxBaseUrl: Input<string>;
+      syncWorkerUrl: Input<string>;
+      appInsightsConnectionString: Input<string>;
+    };
+    flags: Record<string, Input<boolean>>;
+  };
+}
+
+export class SPAWorker extends ComponentResource {
+  wranglerConfig: Output<string>;
+  workerSourceBase64: Output<string>;
+  hostname: Output<string>;
+  name: Output<string>;
+
+  constructor(id: string, args: SPAWorkerArgs, opts?: ResourceOptions) {
+    super(`sprinkle:index:SPAWorker`, id, args, opts);
+
+    this.name = interpolate`${args.name}-${args.env}`;
+    this.hostname = output(args.route);
+
+    const flags = jsonStringify(args.backstage.flags);
+    const workerSource = flags.apply(
+      () => `export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/backstage') {
+      return Response.json({
+        variables: {
+          apiBaseUrl: env.API_BASE_URL,
+          ipxBaseUrl: env.IPX_BASE_URL,
+          syncWorkerUrl: env.SYNC_WORKER_URL,
+          appInsightsConnectionString: env.APP_INSIGHTS_CONNECTION_STRING
+        },
+        flags: JSON.parse(env.BACKSTAGE_FLAGS)
+      });
+    }
+
+    return env.ASSETS.fetch(request);
+  }
+};`
+    );
+
+    this.workerSourceBase64 = workerSource.apply(source => Buffer.from(source, 'utf8').toString('base64'));
+
+    this.wranglerConfig = jsonStringify({
+      compatibility_date: '2026-04-03',
+      workers_dev: false,
+      main: 'worker.generated.js',
+      assets: {
+        directory: args.assetsDirectory,
+        binding: 'ASSETS',
+        not_found_handling: 'single-page-application'
+      },
+      env: {
+        [args.env]: {
+          account_id: args.account_id,
+          name: this.name,
+          routes: [
+            {
+              custom_domain: true,
+              pattern: args.route
+            }
+          ],
+          vars: {
+            API_BASE_URL: args.backstage.variables.apiBaseUrl,
+            IPX_BASE_URL: args.backstage.variables.ipxBaseUrl,
+            SYNC_WORKER_URL: args.backstage.variables.syncWorkerUrl,
+            APP_INSIGHTS_CONNECTION_STRING: args.backstage.variables.appInsightsConnectionString,
+            BACKSTAGE_FLAGS: flags
+          }
+        }
+      }
+    });
+
+    this.registerOutputs({
+      wranglerConfig: this.wranglerConfig,
+      workerSourceBase64: this.workerSourceBase64,
+      hostname: this.hostname,
+      name: this.name
+    });
+  }
+}
