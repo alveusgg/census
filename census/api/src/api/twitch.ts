7deaa00e49f50c3a18e350dad7c2f55318fc7bcc
James <jr_williams@me.com>
2026-06-27 19:52:29 +0100

    Commit WIP API changes (#77)

--- census/api/src/api/twitch.ts
+++ census/api/src/api/twitch.ts
@@ -1,17 +1,18 @@
 import { z } from 'zod';
 import { getClip, getVOD } from '../services/twitch/index.js';
 import { cache, procedure, router } from '../trpc/trpc.js';
-export default router({
-  clip: procedure
-    .input(z.object({ id: z.string() }))
-    .use(cache.query({ key: ({ input }) => ['twitch', 'clip', input.id], ttl: 300 }))
-    .query(async ({ input }) => {
-      return getClip(input.id, 0);
-    }),
-  vod: procedure
-    .input(z.object({ id: z.string() }))
-    .use(cache.query({ key: ({ input }) => ['twitch', 'vod', input.id], ttl: 60 * 60 }))
-    .query(async ({ input }) => {
-      return getVOD(input.id);
-    })
-});
+export const createTwitchRouter = () =>
+  router({
+    clip: procedure
+      .input(z.object({ id: z.string() }))
+      .use(cache.query({ key: ({ input }) => ['twitch', 'clip', input.id], ttl: 300 }))
+      .query(async ({ input }) => {
+        return getClip(input.id, 0);
+      }),
+    vod: procedure
+      .input(z.object({ id: z.string() }))
+      .use(cache.query({ key: ({ input }) => ['twitch', 'vod', input.id], ttl: 60 * 60 }))
+      .query(async ({ input }) => {
+        return getVOD(input.id);
+      })
+  });
